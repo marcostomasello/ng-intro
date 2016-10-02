e0e77723da62405507850244fef7d8103325a0be
Marcos Tomasello <marcos.tomasello@gmail.com>
2016-10-02 17:37:51 -0300

    chapter 3

---
+++ 3-data-binding/data-binding.js
@@ -0,0 +1,9 @@
+
+angular.module('mainModule', [])
+    .controller('MainController', [function() {
+        var self = this;
+
+        self.greeting = "Hiii";
+        self.username = "Maximo Cozzetti";
+        
+     }]);
